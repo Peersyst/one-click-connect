80a1deff86b140c264ce6b51c36fd7588251a5bf
banasa44 <cfbennassar@gmail.com>
2025-06-05 16:56:45 +0200

    feat(docs): Refactor CardWithHeader component structure and update styles for improved layout and responsiveness

--- docs/src/components/CardWithHeader/index.tsx
+++ docs/src/components/CardWithHeader/index.tsx
@@ -13,10 +13,16 @@ interface CardWithHeaderProps {
 
 export function CardWithHeader({ headerImageSrc, headerImageAlt, title, description, links, className }: CardWithHeaderProps): ReactNode {
     return (
-        <div className={clsx(styles.card, className)}>
-            <img src={headerImageSrc} alt={headerImageAlt} className={styles.cardHeaderImage} />
-            <h3 className={styles.cardTitle}>{title}</h3>
-            {description && <p className={styles.cardDescription}>{description}</p>}
+        <div className={clsx("card", styles.customCard, className)}>
+            <div className="card__image">
+                <img src={headerImageSrc} alt={headerImageAlt} className={styles.cardHeaderImage} />
+            </div>
+
+            <div className={styles.cardContent}>
+                <h3 className={styles.cardTitle}>{title}</h3>
+                {description && <p className={styles.cardDescription}>{description}</p>}
+            </div>
+
             <ul className={styles.linkList}>
                 {links.map((link) => (
                     <li key={link.href}>

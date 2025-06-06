b60f0173998ab63f397ccd6a94172a682ef23da3
banasa44 <cfbennassar@gmail.com>
2025-06-06 11:30:32 +0200

    feat(styles): disable scrolling in home page

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -3,25 +3,28 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import { HeroSection } from "../components/HeroSection";
 import HomepageFeatures from "../components/HomepageFeatures";
+import styles from "./index.module.css";
 
 export default function Home(): ReactNode {
     const { siteConfig } = useDocusaurusContext();
 
     return (
-        <Layout
-            title={`Welcome to ${siteConfig.title}`}
-            description="1ClickConnect: Fast, simple, and secure dApp and wallet integration for NEAR."
-        >
-            <main>
-                <HeroSection
-                    title="1ClickConnect"
-                    description="Fast, simple, and secure dApp and wallet integration for NEAR."
-                    imageSrc="img/near-logo.webp"
-                    imageAlt="1ClickConnect Logo"
-                >
-                    <HomepageFeatures />
-                </HeroSection>
-            </main>
-        </Layout>
+        <div className={styles.layout}>
+            <Layout
+                title={`Welcome to ${siteConfig.title}`}
+                description="1ClickConnect: Fast, simple, and secure dApp and wallet integration for NEAR."
+            >
+                <main>
+                    <HeroSection
+                        title="1ClickConnect"
+                        description="Fast, simple, and secure dApp and wallet integration for NEAR."
+                        imageSrc="img/near-logo.webp"
+                        imageAlt="1ClickConnect Logo"
+                    >
+                        <HomepageFeatures />
+                    </HeroSection>
+                </main>
+            </Layout>
+        </div>
     );
 }

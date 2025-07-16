f76f4268dbbedb422911c974d6b5acc493e98a70
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2025-07-16 15:14:36 +0100

    fix(core): mock imports

--- packages/shared/core/test/mocks/core/fak-sign.msg.global-mock.ts
+++ packages/shared/core/test/mocks/core/fak-sign.msg.global-mock.ts
@@ -1,6 +1,6 @@
-import { MsgFakSign } from "@one-click-connect/core";
 import { createGlobalMock, MethodMock } from "@shared/test";
 import { TransactionMock } from "../near";
+import { MsgFakSign } from "../../../src";
 
 export const MsgFakSignGlobalMock = createGlobalMock(MsgFakSign.prototype, {
     toURL: new MethodMock("mockReturnValue", "mockURL"),
